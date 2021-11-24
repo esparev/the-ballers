7ad3b2ec6d95ea55f00e4dbd022189d8723cf518
JoseMa Esparev <esparev@hotmail.com>
2021-11-24 11:22:25 -0600

    create: Administrators page

--- src/frontend/containers/Admins.jsx
+++ src/frontend/containers/Admins.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import Actor from '../components/Actor.jsx';
+import ButtonContainer from './ButtonContainer.jsx';
+import YellowButton from '../components/YellowButton.jsx';
+import '../assets/styles/components/TeamPlayers.scss';
+import userIcon from '../assets/icons/user-icon.svg';
 
 const Admins = () => {
-  return;
+  return (
+    <main className='admins'>
+      <div className='players-coach'>
+        <section className='actors'>
+          <div className='actors__container'>
+            <h2 className='actors__container--title'>Administradores</h2>
+            <div className='more-actors'>
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+              <Actor
+                name='Nombre Administrador'
+                image={userIcon}
+                route='/admins/admin'
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <ButtonContainer>
+        <YellowButton name='Nuevo Administrador' route='/admins/nuevo-admin' />
+      </ButtonContainer>
+    </main>
+  );
 };
 
 export default Admins;
--- src/frontend/routes/App.jsx
+++ src/frontend/routes/App.jsx
@@ -9,6 +9,7 @@ import Tournament from '../containers/Tournament.jsx';
 import Leagues from '../containers/Leagues.jsx';
 import LeagueTeams from '../containers/LeagueTeams.jsx';
 import TeamPlayers from '../containers/TeamPlayers.jsx';
+import Admins from '../containers/Admins.jsx';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route exact path='/ligas' component={Leagues} />
           <Route exact path='/ligas/liga' component={LeagueTeams} />
           <Route exact path='/ligas/liga/equipo' component={TeamPlayers} />
+          <Route exact path='/admins' component={Admins} />
         </Layout>
       </Switch>
     </BrowserRouter>
