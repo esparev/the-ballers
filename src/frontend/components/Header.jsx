e0a5cedf19e525fe5fb3f5907d89dbc632940d6d
JoseMa Esparev <esparev@hotmail.com>
2021-12-01 09:43:54 -0600

    refactor: Add profile dropdown menu

--- src/frontend/components/Header.jsx
+++ src/frontend/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 import beismichLogo from '../assets/static/beismich-logo.png';
@@ -6,43 +6,93 @@ import userIcon from '../assets/icons/user-icon.svg';
 import burgerMenuIcon from '../assets/icons/burger-menu-icon.svg';
 
 const Header = () => {
-  const menuDisplay = () => {
-    var el = document.getElementById('nav');
-    if (el.style.display === 'block') {
-      el.style.display = 'none';
+  useEffect(() => {
+    var nav = document.getElementById('nav');
+
+    const menuDesktop = () => {
+      if (screen.width > 836) {
+        nav.style.display = 'block';
+      }
+    };
+
+    nav.addEventListener('resize', menuDesktop);
+  }, []);
+
+  const adminDisplay = () => {
+    var adminNav = document.getElementById('admin-nav');
+    if (adminNav.style.display === 'none' || adminNav.style.display === '') {
+      adminNav.style.display = 'block';
     } else {
-      el.style.display = 'block';
+      adminNav.style.display = 'none';
+    }
+  };
+
+  const menuDisplay = () => {
+    var nav = document.getElementById('nav');
+    if (nav.style.display === 'none' || nav.style.display === '') {
+      nav.style.display = 'block';
+    } else {
+      nav.style.display = 'none';
     }
   };
 
   return (
-    <header>
+    <header className='main-header'>
       <Link className='header--logo' to='/'>
         <img src={beismichLogo} alt='Logo BEISMICH' />
       </Link>
       <div className='header__menu'>
-        <a className='header--burger-menu' onClick={menuDisplay}>
+        <a
+          className='header--burger-menu'
+          onClick={menuDisplay}
+          onChange={menuDisplay}
+        >
           <img src={burgerMenuIcon} alt='Menu' />
         </a>
-        <nav className='header__nav' id='nav'>
+        <nav className='header__nav nav' id='nav'>
           <ul className='header__nav-list'>
-            <li className='header__nav-list--item'>
-              <Link to='/noticias'>Noticias</Link>
+            <li className='header__nav-list--item user-list'>
+              <Link to='/noticias' onClick={menuDisplay}>
+                Noticias
+              </Link>
             </li>
-            <li className='header__nav-list--item'>
-              <Link to='/torneos'>Torneos</Link>
+            <li className='header__nav-list--item user-list'>
+              <Link to='/torneos' onClick={menuDisplay}>
+                Torneos
+              </Link>
             </li>
-            <li className='header__nav-list--item'>
-              <Link to='/ligas'>Ligas</Link>
+            <li className='header__nav-list--item user-list'>
+              <Link to='/ligas' onClick={menuDisplay}>
+                Ligas
+              </Link>
             </li>
-            <li className='header__nav-list--item'>
-              <Link to='/conocenos-pending'>Conócenos</Link>
+            <li className='header__nav-list--item user-list'>
+              <Link to='/conocenos-pending' onClick={menuDisplay}>
+                Conócenos
+              </Link>
             </li>
-            <li className='header__nav-list--item'>
-              <a href='#'>
+            <li className='header__nav-list--item header__nav-list--image'>
+              <a onClick={adminDisplay}>
                 <img src={userIcon} alt='Usuario' />
               </a>
             </li>
+            <div className='header__nav-admin admin-nav' id='admin-nav'>
+              <li className='header__nav-list--item'>
+                <Link to='/perfil' onClick={adminDisplay}>
+                  Perfil
+                </Link>
+              </li>
+              <li className='header__nav-list--item'>
+                <Link to='/admins' onClick={adminDisplay}>
+                  Administradores
+                </Link>
+              </li>
+              <li className='header__nav-list--item'>
+                <Link to='/iniciar-sesion' onClick={adminDisplay}>
+                  Iniciar sesión
+                </Link>
+              </li>
+            </div>
           </ul>
         </nav>
       </div>
