85269c81dcb9261dc433abb18792f4af8ce4e3d2
JoseMa Esparev <esparev@hotmail.com>
2021-12-20 16:22:53 -0600

    feature: Admin that is not a hero cannot edit other admins

--- src/frontend/containers/Admin.jsx
+++ src/frontend/containers/Admin.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import MoreActors from '../components/MoreActors.jsx';
+import MoreActors from '../components/MoreActors';
 import ButtonContainer from './ButtonContainer';
 import GrayButton from '../components/GrayButton';
 import '../assets/styles/components/ActorContainer.scss';
 import userIcon from '../assets/icons/user-icon.svg';
 
-const Admin = ({admin}) => {
+const Admin = ({ admin }) => {
   useEffect(() => {
     document.title = 'BEISMICH • Administrador';
     window.scrollTo(0, 0);
@@ -17,14 +17,15 @@ const Admin = ({admin}) => {
         <div className='actor__container'>
           <img
             className='actor__container--image'
-            src={admin.image}
+            src={userIcon}
             alt='Foto del administrador'
           />
           <div className='actor__info'>
-            <h1 className='actor__info--name'>{admin.name}</h1>
+            <h1 className='actor__info--name'>Juan</h1>
             <div className='actor__info-about'>
               <p>
-                <strong>Correo electrónico: </strong>{admin.email}
+                <strong>Correo electrónico: </strong>
+                admin@mail.com
               </p>
             </div>
           </div>
@@ -69,10 +70,12 @@ const Admin = ({admin}) => {
         </section>
 
         <ButtonContainer>
-          <GrayButton
-            name='Editar Administrador'
-            route='/admins/admin/editar-admin'
-          />
+          {localStorage.getItem('role') === 'hero' ? (
+            <GrayButton
+              name='Editar Administrador'
+              route='/admins/admin/editar-admin'
+            />
+          ) : null}
         </ButtonContainer>
       </section>
     </main>
