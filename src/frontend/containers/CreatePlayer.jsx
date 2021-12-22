e9291feebb8058c045d166f31ef6c64f10175f4b
JoseMa Esparev <esparev@hotmail.com>
2021-12-22 14:47:07 -0600

    feature: Create player action belonging to its respective team and league

--- src/frontend/containers/CreatePlayer.jsx
+++ src/frontend/containers/CreatePlayer.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from 'react';
-import ButtonContainer from './ButtonContainer.jsx';
-import YellowButton from '../components/YellowButton.jsx';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Message from '../components/Message';
+import ButtonContainer from './ButtonContainer';
+import ImageUploader from '../functions/ImageUploader';
+import updateThumbnail from '../functions/updateThumbnail';
 import '../assets/styles/components/CreateEntity.scss';
-import updateThumbnail from '../functions/updateThumbnail.js';
+
+const API = 'https://beismich.herokuapp.com/api/v1';
 
 const CreatePlayer = () => {
   useEffect(() => {
@@ -33,107 +38,203 @@ const CreatePlayer = () => {
     document.querySelectorAll('.form__image--input').forEach((inputElement) => {
       const dropZoneElement = inputElement.closest('.form__image');
 
-      dropZoneElement.addEventListener('click', (e) => {
-        inputElement.click();
-      });
-
+      /**
+       * Updates the image thumbnail after change is detected in the
+       * image drop zone
+       */
       inputElement.addEventListener('change', (e) => {
         if (inputElement.files.length) {
           updateThumbnail(dropZoneElement, inputElement.files[0]);
         }
       });
 
-      /* Event listener when file is being dragged over the drop zone
-      Activates CSS indicator to let the user know that they have
-      dragged the item over the drop zone  */
+      /**
+       * Event listener when file is being dragged over the drop zone
+       * Activates CSS indicator to let the user know that they have
+       * dragged the item over the drop zone
+       */
       dropZoneElement.addEventListener('dragover', (e) => {
         e.preventDefault();
         dropZoneElement.classList.add('drop-zone__over');
       });
 
-      // Events
+      /**
+       * Event listener when the file isn't being dragged
+       */
       ['dragleave', 'dragend'].forEach((type) => {
         dropZoneElement.addEventListener(type, (e) => {
           dropZoneElement.classList.remove('drop-zone__over');
         });
       });
-
-      // Event listener after the file has been dropped on the drop zone
-      dropZoneElement.addEventListener('drop', (e) => {
-        e.preventDefault();
-        if (e.dataTransfer.files.length) {
-          inputElement.files = e.dataTransfer.files;
-          updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-        }
-
-        dropZoneElement.classList.remove('drop-zone__over');
-      });
     });
   }, []);
 
-  return (
-    <main className='create-container'>
-      <form className='form' action=''>
-        <h1 className='form--title'>Agregar Nuevo Jugador</h1>
-        <input className='input' type='text' placeholder='Nombre *' required />
-        <select className='input empty' name='positions' id='positions'>
-          <option defaultValue value=''>
-            Posición
-          </option>
-          <option value='Pitcher'>Pitcher</option>
-          <option value='Catcher'>Catcher</option>
-          <option value='Primera Base'>Primera base</option>
-          <option value='Segunda Base'>Segunda base</option>
-          <option value='Tercera Base'>Tercera base</option>
-          <option value='Campocorto'>Campocorto</option>
-          <option value='Jardinero Izquierdo'>Jardinero izquierdo</option>
-          <option value='Jardinero Central'>Jardinero central</option>
-          <option value='Jardinero Derecho'>Jardinero derecho</option>
-          <option value='Bateador'>Bateador</option>
-        </select>
-        <label className='form--label label' htmlFor='date'>
-          Fecha de nacimiento
-        </label>
-        <input
-          className='input empty'
-          type='date'
-          id='date'
-          placeholder='Fecha de nacimiento'
-        />
+  /**
+   * Re assigns the form image value after a storage event
+   * has been dispatched, which means that an image has been
+   * uploaded to the app
+   */
+  window.onstorage = () => {
+    form.cover = localStorage.getItem('uploaded image');
+  };
 
-        <label className='form--label label' htmlFor='file'>
-          Foto del Jugador
-        </label>
-        <div className='form__image form__image-square'>
+  /**
+   * Sets the initial values for the form fields
+   */
+  const [form, setValues] = useState({
+    name: '',
+    position: '',
+    birthday: '',
+    image: localStorage.getItem('actor image'),
+    teamId: localStorage.getItem('selected team'),
+  });
+
+  /**
+   * Sets values after onChange event is triggered on the
+   * indicated inputs
+   */
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  /**
+   * Authorization header configuration for API request
+   */
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
+
+  /**
+   * Sends a post request to the URL of the API provided
+   * with the data entered by the user in a form along
+   * with a bearer token included in the headers configuration
+   * @param {*} url - API URL
+   * @param {*} data - body data to post
+   * @param {*} config - headers configuration
+   */
+  const addPlayer = async (url, data, config) => {
+    await axios
+      .post(url, data, config)
+      .then((res) => {
+        ReactDOM.render(
+          <Message
+            message='¡Jugador registrado con éxito!'
+            messageStatus='success'
+          />,
+          document.getElementById('message-container')
+        );
+
+        localStorage.removeItem('uploaded image');
+      })
+      .catch((error) => {
+        ReactDOM.render(
+          <Message
+            message='¡Ups!, Hubo un error al registrar el jugador'
+            messageStatus='error'
+          />,
+          document.getElementById('message-container')
+        );
+
+        localStorage.removeItem('uploaded image');
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addPlayer(`${API}/jugadores`, form, config);
+  };
+
+  return (
+    <>
+      <div id='message-container'></div>
+
+      <main className='create-container'>
+        <form className='form' onSubmit={handleSubmit}>
+          <h1 className='form--title'>Agregar Nuevo Jugador</h1>
           <input
-            className='form__image--input form__image-square--input'
-            type='file'
-            id='file'
+            className='input'
+            name='name'
+            type='text'
+            placeholder='Nombre *'
+            required
+            onChange={handleInput}
           />
-          <div className='form__image-labels form__image-square-labels'>
-            <span
-              className='
+          <select
+            className='input empty'
+            name='position'
+            id='positions'
+            onChange={handleInput}
+          >
+            <option defaultValue value=''>
+              Posición
+            </option>
+            <option value='Pitcher'>Pitcher</option>
+            <option value='Catcher'>Catcher</option>
+            <option value='Primera Base'>Primera base</option>
+            <option value='Segunda Base'>Segunda base</option>
+            <option value='Tercera Base'>Tercera base</option>
+            <option value='Campocorto'>Campocorto</option>
+            <option value='Jardinero Izquierdo'>Jardinero izquierdo</option>
+            <option value='Jardinero Central'>Jardinero central</option>
+            <option value='Jardinero Derecho'>Jardinero derecho</option>
+            <option value='Bateador'>Bateador</option>
+          </select>
+          <label className='form--label label' htmlFor='date'>
+            Fecha de nacimiento
+          </label>
+          <input
+            className='input empty'
+            name='birthday'
+            type='date'
+            id='date'
+            placeholder='Fecha de nacimiento'
+            onChange={handleInput}
+          />
+
+          <label className='form--label label' htmlFor='file'>
+            Foto del Jugador
+          </label>
+          <div className='form__image form__image-square' id='drop-zone'>
+            <input
+              className='form__image--input form__image-square--input'
+              type='file'
+              id='file'
+              accept='image/*'
+            />
+            <div className='form__image-labels form__image-square-labels'>
+              <span
+                className='
                 form__image--label form__image-square--label
                 drop-zone--prompt
               '
-            >
-              Arrastra una imagen
-            </span>
-            <span
-              className='
+              >
+                Arrastra una imagen
+              </span>
+              <span
+                className='
                 form__image--label-button form__image-square--label-button
                 drop-zone--prompt
               '
-            >
-              O haz clic aquí para subir una imagen
-            </span>
+              >
+                O haz clic aquí para subir una imagen
+              </span>
+            </div>
           </div>
-        </div>
-        <ButtonContainer>
-          <YellowButton name='Agregar Jugador' />
-        </ButtonContainer>
-      </form>
-    </main>
+          <ButtonContainer>
+            <button type='submit' className='button yellow-button'>
+              Agregar Jugador
+            </button>
+          </ButtonContainer>
+        </form>
+      </main>
+
+      <ImageUploader />
+    </>
   );
 };
 
