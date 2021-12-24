ebd86b7dc1c8cd571f317a677c6e68dd2ce27771
JoseMa Esparev <esparev@hotmail.com>
2021-12-23 18:48:59 -0600

    feature: Add structured data and descriptive meta tags for SEO

--- src/frontend/containers/SingleNews.jsx
+++ src/frontend/containers/SingleNews.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import Helmet from 'react-helmet';
 import Article from '../components/Article';
 import ButtonContainer from './ButtonContainer';
 import GrayButton from '../components/GrayButton';
 import useGetSingleNews from '../hooks/useGetSingleNews';
 import useGetNews from '../hooks/useGetNews';
+import sortByDate from '../utils/functions/sortByDate';
 import { envConfig } from '../utils/config';
 import '../assets/styles/components/Article.scss';
 import linkIcon from '../assets/icons/link-icon.svg';
@@ -15,20 +17,12 @@ const SingleNews = (props) => {
   const { id } = props.match.params;
 
   moment.locale('es');
+  
   const news = useGetSingleNews(envConfig.apiUrl, id);
   let newsCollection = useGetNews(envConfig.apiUrl);
+
   localStorage.setItem('selected news', news.id);
 
-  /**
-   * Sorts the array of objects by recent date
-   * @param {*} arr - array of objects
-   */
-  const sortByDate = (arr) => {
-    const sorter = (a, b) => {
-      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-    };
-    arr.sort(sorter);
-  };
   sortByDate(newsCollection);
 
   newsCollection = newsCollection.slice(0, 3);
@@ -38,80 +32,135 @@ const SingleNews = (props) => {
     window.scrollTo(0, 0);
   }, []);
 
+  const articleStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: news.title,
+    image: news.cover,
+    author: news.author,
+    genre: 'beisbol',
+    keywords: 'beisbol torneo michoacan',
+    publisher: {
+      '@type': 'Organization',
+      url: 'https://www.beismich.netlify.app',
+      name: 'BEISMICH',
+    },
+    url: 'https://www.beismich.netlify.app',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': 'https://google.com/article',
+    },
+    datePublished: news.createdAt,
+    dateCreated: news.createdAt,
+    description: news.description,
+    articleBody: news.description,
+  };
+
   return (
-    <main className='article__container'>
-      <section className='article'>
-        <div className='article__cover'>
-          <img
-            className='article__cover--image cover-image'
-            src={news.cover}
-            alt='Imagen de la noticia'
-          />
-        </div>
+    <>
+      <Helmet>
+        <meta property='og:locale' content='es_ES' />
+        <meta property='og:type' content='article' />
+        <meta property='og:title' content={news.title} />
+        <meta property='og:description' content={news.description} />
+        <meta property='og:url' content={window.location} />
+        <meta property='og:site_name' content='BEISMICH' />
+        <meta property='article:author' content={news.author} />
+        <meta
+          property='article:publisher'
+          content='https://www.facebook.com/BEISMICH'
+        />
+        <meta property='og:image' content={news.cover} />
 
-        <h1 className='article--title'>{news.title}</h1>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={news.title} />
+        <meta name='twitter:description' content={news.description} />
+        <meta name='twitter:domain' content='BEISMICH' />
+        <meta name='twitter:image:src' content={news.cover} />
 
-        <div className='article--info'>
-          <p>{news.author}</p>
-          <p>•</p>
-          <p>
-            {moment(news.createdAt).format('DD [de] MMMM [de] YYYY, h:mm a')}
-          </p>
-        </div>
+        <script type='application/ld+json'>
+          {JSON.stringify(articleStructuredData)}
+        </script>
+      </Helmet>
 
-        <hr className='article--line' />
-
-        <p className='article--description'>{news.description}</p>
-
-        <div className='article__share'>
-          <p>Compartir:</p>
-          <div className='article__icons'>
-            <a href='#'>
-              <img
-                className='share-icon'
-                src={facebookIcon}
-                alt='Compartir en Facebook'
-              />
-            </a>
-            <a href='#'>
-              <img
-                className='share-icon'
-                src={twitterIcon}
-                alt='Compartir en Twitter'
-              />
-            </a>
-            <a href='#'>
-              <img className='share-icon' src={linkIcon} alt='Copiar enlace' />
-            </a>
-          </div>
-        </div>
-
-        {localStorage.getItem('id') ? (
-          <ButtonContainer>
-            <GrayButton
-              name='Editar Noticia'
-              route={`/noticias/noticia/${news.id}/editar-noticia`}
+      <main className='article__container'>
+        <section className='article'>
+          <div className='article__cover'>
+            <img
+              className='article__cover--image cover-image'
+              src={news.cover}
+              alt='Imagen de la noticia'
             />
-          </ButtonContainer>
-        ) : null}
-      </section>
+          </div>
 
-      <section className='more-articles'>
-        <h2 className='more-articles--title'>Más Noticias</h2>
+          <h1 className='article--title'>{news.title}</h1>
 
-        {newsCollection.map((news) => (
-          <Article
-            news={news}
-            key={news.id}
-            title={news.title}
-            cover={news.cover}
-            date={moment(news.createdAt).format('DD MMMM, YYYY')}
-            category='Noticia'
-            route={`/noticias/noticia/${news.id}`}
-          />
-        ))}
-      </section>
-    </main>
+          <div className='article--info'>
+            <p>{news.author}</p>
+            <p>•</p>
+            <p>
+              {moment(news.createdAt).format('DD [de] MMMM [de] YYYY, h:mm a')}
+            </p>
+          </div>
+
+          <hr className='article--line' />
+
+          <p className='article--description'>{news.description}</p>
+
+          <div className='article__share'>
+            <p>Compartir:</p>
+            <div className='article__icons'>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={facebookIcon}
+                  alt='Compartir en Facebook'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={twitterIcon}
+                  alt='Compartir en Twitter'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={linkIcon}
+                  alt='Copiar enlace'
+                />
+              </a>
+            </div>
+          </div>
+
+          {localStorage.getItem('id') ? (
+            <ButtonContainer>
+              <GrayButton
+                name='Editar Noticia'
+                route={`/noticias/noticia/${news.id}/editar-noticia`}
+              />
+            </ButtonContainer>
+          ) : null}
+        </section>
+
+        <section className='more-articles'>
+          <h2 className='more-articles--title'>Más Noticias</h2>
+
+          {newsCollection.map((news) => (
+            <Article
+              news={news}
+              key={news.id}
+              title={news.title}
+              cover={news.cover}
+              date={moment(news.createdAt).format('DD MMMM, YYYY')}
+              category='Noticia'
+              route={`/noticias/noticia/${news.id}`}
+            />
+          ))}
+        </section>
+      </main>
+    </>
   );
 };
 
--- src/frontend/containers/Tournament.jsx
+++ src/frontend/containers/Tournament.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import Helmet from 'react-helmet';
 import Article from '../components/Article';
 import ButtonContainer from './ButtonContainer';
 import GrayButton from '../components/GrayButton';
@@ -23,7 +24,7 @@ const Tournament = (props) => {
   localStorage.setItem('selected tournament', tournament.id);
 
   sortByDate(tournaments);
-  
+
   tournaments = tournaments.slice(0, 3);
 
   useEffect(() => {
@@ -31,93 +32,148 @@ const Tournament = (props) => {
     window.scrollTo(0, 0);
   }, []);
 
+  const articleStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: tournament.title,
+    image: tournament.cover,
+    author: tournament.author,
+    genre: 'beisbol',
+    keywords: 'beisbol torneo michoacan',
+    publisher: {
+      '@type': 'Organization',
+      name: 'BEISMICH',
+      url: 'https://www.beismich.netlify.app',
+    },
+    url: 'https://www.beismich.netlify.app',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': 'https://google.com/article',
+    },
+    datePublished: tournament.createdAt,
+    dateCreated: tournament.createdAt,
+    description: tournament.link,
+    articleBody: tournament.link,
+  };
+
   return (
-    <main className='article__container'>
-      <section className='article'>
-        {tournament.cover ? (
-          <div className='article__cover'>
-            <img
-              className='article__cover--image cover-image'
-              src={tournament.cover}
-              alt='Imagen del torneo'
-            />
+    <>
+      <Helmet>
+        <meta property='og:locale' content='es_ES' />
+        <meta property='og:type' content='article' />
+        <meta property='og:title' content={tournament.title} />
+        <meta property='og:description' content={tournament.link} />
+        <meta property='og:url' content={window.location} />
+        <meta property='og:site_name' content='BEISMICH' />
+        <meta property='article:author' content={tournament.author} />
+        <meta
+          property='article:publisher'
+          content='https://www.facebook.com/BEISMICH'
+        />
+        <meta property='og:image' content={tournament.cover} />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={tournament.title} />
+        <meta name='twitter:description' content={tournament.description} />
+        <meta name='twitter:domain' content='BEISMICH' />
+        <meta name='twitter:image:src' content={tournament.cover} />
+
+        <script type='application/ld+json'>
+          {JSON.stringify(articleStructuredData)}
+        </script>
+      </Helmet>
+
+      <main className='article__container'>
+        <section className='article'>
+          {tournament.cover ? (
+            <div className='article__cover'>
+              <img
+                className='article__cover--image cover-image'
+                src={tournament.cover}
+                alt='Imagen del torneo'
+              />
+            </div>
+          ) : null}
+
+          <h1 className='article--title'>{tournament.title}</h1>
+
+          <div className='article--info'>
+            <p>{tournament.author}</p>
+            <p>•</p>
+            <p>
+              {moment(tournament.createdAt).format(
+                'DD [de] MMMM [de] YYYY, h:mm a'
+              )}
+            </p>
           </div>
-        ) : null}
 
-        <h1 className='article--title'>{tournament.title}</h1>
+          <hr className='article--line' />
 
-        <div className='article--info'>
-          <p>{tournament.author}</p>
-          <p>•</p>
-          <p>
-            {moment(tournament.createdAt).format(
-              'DD [de] MMMM [de] YYYY, h:mm a'
-            )}
+          <p className='article--description'>
+            <a
+              className='card--link'
+              href={tournament.link}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {tournament.link}
+            </a>
           </p>
-        </div>
 
-        <hr className='article--line' />
-
-        <p className='article--description'>
-          <a
-            className='card--link'
-            href={tournament.link}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            {tournament.link}
-          </a>
-        </p>
-
-        <div className='article__share'>
-          <p>Compartir:</p>
-          <div className='article__icons'>
-            <a href='#'>
-              <img
-                className='share-icon'
-                src={facebookIcon}
-                alt='Compartir en Facebook'
-              />
-            </a>
-            <a href='#'>
-              <img
-                className='share-icon'
-                src={twitterIcon}
-                alt='Compartir en Twitter'
-              />
-            </a>
-            <a href='#'>
-              <img className='share-icon' src={linkIcon} alt='Copiar enlace' />
-            </a>
+          <div className='article__share'>
+            <p>Compartir:</p>
+            <div className='article__icons'>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={facebookIcon}
+                  alt='Compartir en Facebook'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={twitterIcon}
+                  alt='Compartir en Twitter'
+                />
+              </a>
+              <a href='#'>
+                <img
+                  className='share-icon'
+                  src={linkIcon}
+                  alt='Copiar enlace'
+                />
+              </a>
+            </div>
           </div>
-        </div>
 
-        {localStorage.getItem('id') ? (
-          <ButtonContainer>
-            <GrayButton
-              name='Editar Torneo'
-              route={`/torneos/torneo/${tournament.id}/editar-torneo`}
+          {localStorage.getItem('id') ? (
+            <ButtonContainer>
+              <GrayButton
+                name='Editar Torneo'
+                route={`/torneos/torneo/${tournament.id}/editar-torneo`}
+              />
+            </ButtonContainer>
+          ) : null}
+        </section>
+
+        <section className='more-articles'>
+          <h2 className='more-articles--title'>Más Torneos</h2>
+
+          {tournaments.map((tournament) => (
+            <Article
+              tournament={tournament}
+              key={tournament.id}
+              title={tournament.title}
+              cover={tournament.cover}
+              date={moment(tournament.createdAt).format('DD MMMM, YYYY')}
+              category='Torneo'
+              route={`/torneos/torneo/${tournament.id}`}
             />
-          </ButtonContainer>
-        ) : null}
-      </section>
-
-      <section className='more-articles'>
-        <h2 className='more-articles--title'>Más Torneos</h2>
-
-        {tournaments.map((tournament) => (
-          <Article
-            tournament={tournament}
-            key={tournament.id}
-            title={tournament.title}
-            cover={tournament.cover}
-            date={moment(tournament.createdAt).format('DD MMMM, YYYY')}
-            category='Torneo'
-            route={`/torneos/torneo/${tournament.id}`}
-          />
-        ))}
-      </section>
-    </main>
+          ))}
+        </section>
+      </main>
+    </>
   );
 };
 
