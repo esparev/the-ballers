51035637b64ef61b2c4abb065c85e0e9d52cc97d
JoseMa Esparev <esparev@hotmail.com>
2021-12-26 23:11:18 -0600

    refactor: Remove unused files

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
-import reducer from './reducers';
-import App from './routes/App.jsx';
-
-const store = createStore(reducer);
+import App from './routes/App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App isLogged={localStorage.getItem('id')} />
-  </Provider>,
+  <App isLogged={localStorage.getItem('id')} />,
   document.getElementById('app')
 );
--- src/frontend/utils/config/index.js
+++ src/frontend/utils/config/index.js
@@ -5,18 +5,10 @@
  * @property {*} isProd - verify environment type production
  * @property {*} port - server port
  * @property {*} apiUrl - api url
- * @property {*} apiKey - api key token
- * @property {*} jwtSecret - jsonwebtoken secret
- * @property {*} smtpPassword - smtp email address
- * @property {*} smtpPassword - smtp email password
  */
 export const envConfig = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   apiUrl: process.env.API_URL,
-  apiKey: process.env.API_KEY,
-  jwtSecret: process.env.JWT_SECRET,
-  smtpEmail: process.env.SMTP_EMAIL,
-  smtpPassword: process.env.SMTP_PASSWORD,
 };
