94f63dbb5f4314aa3b1ab37c4068f016866b25cc
JoseMa <esparev@hotmail.com>
2023-02-16 19:14:49 -0600

    refactor: ♻️ re-design the news/tournaments card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import YellowButton from '@components/YellowButton';
+import PrimaryButton from '@components/PrimaryButton';
 import '@styles/Card.scss';
 // ---------------------------------------- END OF IMPORTS
 
 /**
- * Creates the cards component with all its functions 
+ * Creates the cards component with all its functions
  * stored inside for its full operation
- * @param {*} props 
+ * @param {*} props
  * @returns JSX code to render to the DOM tree
  */
 const Card = (props) => {
@@ -15,32 +15,25 @@ const Card = (props) => {
 
   return (
     <div className='card'>
-      {cover ? (
-        <img className='card--image' src={cover} alt='Imagen de portada' />
-      ) : null}
+      {cover ? <img className='card--image' src={cover} alt='Cover of the news' /> : null}
 
       <div className='card__info-container'>
         <div className='card__info'>
           <h2 className='card__info--title'>{title}</h2>
-          <div className='card__about'>
+          <div className='card__meta-info'>
             <p>{date}</p>
             <p>•</p>
-            <p className='card--category'>{category}</p>
+            <p className='card__meta-info--category'>{category}</p>
           </div>
           <p className='card--description'>
             {description}
-            <a
-              className='card--link'
-              href={link}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
+            <a className='card--link' href={link} target='_blank' rel='noopener noreferrer'>
               {link}
             </a>
           </p>
         </div>
 
-        <YellowButton name='Ver más' route={route} />
+        <PrimaryButton name='See more' route={route} />
       </div>
     </div>
   );
