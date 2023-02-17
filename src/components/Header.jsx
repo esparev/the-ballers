a2962141f8635d4171e539cf26599131e218ca98
JoseMa <esparev@hotmail.com>
2023-02-16 18:05:14 -0600

    refactor: ♻️ re-design the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '@styles/Header.scss';
-// import beismichLogo from '@static/beismich-logo.png';
-import userIcon from '@icons/user-icon.svg';
+import ballersLogo from '@static/ballers-logo.png';
 import burgerMenuIcon from '@icons/burger-menu-icon.svg';
 // ---------------------------------------- END OF IMPORTS
 
@@ -47,68 +46,73 @@ const Header = () => {
   };
 
   return (
-    <header className='main-header' id='header'>
-      <Link className='header--logo' to='/'>
-        <img src='' alt='Logo BEISMICH' />
-      </Link>
-      <div className='header__menu'>
+    <header id='header'>
+      <div className='header__main'>
+        <Link className='header__logo' to='/'>
+          <img src={ballersLogo} alt="The Ballers' Logo" />
+        </Link>
         <a className='header--burger-menu' onClick={menuDisplay}>
           <img src={burgerMenuIcon} alt='Menu' />
         </a>
-        <nav className='header__nav nav' id='nav'>
-          <ul className='header__nav-list'>
-            <li className='header__nav-list--item user-list'>
-              <Link to='/noticias'>Noticias</Link>
+      </div>
+      <nav className='nav' id='nav'>
+        <ul className='nav__list'>
+          <li className='nav__list--item'>
+            <Link to='/noticias'>News</Link>
+          </li>
+          <li className='nav__list--item'>
+            <Link to='/torneos'>Tournaments</Link>
+          </li>
+          <li className='nav__list--item'>
+            <Link to='/ligas'>Clubs</Link>
+          </li>
+          <li className='nav__list--item'>
+            <Link to='/conocenos'>About us</Link>
+          </li>
+          {localStorage.getItem('image') ? (
+            <li className='nav__list--image'>
+              <a onClick={adminDisplay}>
+                <img src={localStorage.getItem('image')} alt="User's profile pic" />
+              </a>
             </li>
-            <li className='header__nav-list--item user-list'>
-              <Link to='/torneos'>Torneos</Link>
+          ) : (
+            <li className='nav__list--item nav__list--image'>
+              <a onClick={adminDisplay}>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 499.14 499.14'>
+                  <path
+                    d='M250,.21C112.17.21.43,112,.43,249.79S112.17,499.36,250,499.36,499.57,387.62,499.57,249.79,387.83.21,250,.21Zm0,67.39a83.34,83.34,0,1,1-83.33,83.33A83.33,83.33,0,0,1,250,67.6Zm-1.25,382c-78.18,0-161.29-50.06-157.6-87.83,3.67-37.5,92.75-61.27,157.6-61.27s153.93,23.77,157.6,61.27C410,399.49,326.93,449.55,248.75,449.55Z'
+                    transform='translate(-0.43 -0.21)'
+                  />
+                </svg>
+              </a>
             </li>
-            <li className='header__nav-list--item user-list'>
-              <Link to='/ligas'>Ligas</Link>
-            </li>
-            <li className='header__nav-list--item user-list'>
-              <Link to='/conocenos'>Conócenos</Link>
-            </li>
-            {localStorage.getItem('image') ? (
-              <li className='header__nav-list--admin header__nav-list--image'>
-                <a onClick={adminDisplay}>
-                  <img src={localStorage.getItem('image')} alt='Usuario' />
-                </a>
+          )}
+          <div className='admin-nav' id='admin-nav'>
+            {localStorage.getItem('id') ? (
+              <>
+                <li className='nav__list--item'>
+                  <Link to='/perfil'>Profile</Link>
+                </li>
+                <li className='nav__list--item'>
+                  <Link to='/admins'>Admins</Link>
+                </li>
+              </>
+            ) : null}
+
+            {localStorage.getItem('id') ? (
+              <li className='nav__list--item'>
+                <Link to='/iniciar-sesion' onClick={logout}>
+                  Logout
+                </Link>
               </li>
             ) : (
-              <li className='header__nav-list--item header__nav-list--image'>
-                <a onClick={adminDisplay}>
-                  <img src={userIcon} alt='Usuario' />
-                </a>
+              <li className='nav__list--item'>
+                <Link to='/iniciar-sesion'>Login</Link>
               </li>
             )}
-            <div className='header__nav-admin admin-nav' id='admin-nav'>
-              {localStorage.getItem('id') ? (
-                <>
-                  <li className='header__nav-list--item'>
-                    <Link to='/perfil'>Perfil</Link>
-                  </li>
-                  <li className='header__nav-list--item'>
-                    <Link to='/admins'>Administradores</Link>
-                  </li>
-                </>
-              ) : null}
-
-              {localStorage.getItem('id') ? (
-                <li className='header__nav-list--item'>
-                  <Link to='/iniciar-sesion' onClick={logout}>
-                    Cerrar sesión
-                  </Link>
-                </li>
-              ) : (
-                <li className='header__nav-list--item'>
-                  <Link to='/iniciar-sesion'>Iniciar sesión</Link>
-                </li>
-              )}
-            </div>
-          </ul>
-        </nav>
-      </div>
+          </div>
+        </ul>
+      </nav>
     </header>
   );
 };
