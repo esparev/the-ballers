e9fba29b129756f02e9fd8dbfea74fcf8c60ff99
JoseMa <esparev@hotmail.com>
2023-02-17 21:58:46 -0600

    patch: 🩹 modify copyright year to update automatically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import '@styles/Footer.scss';
 import ballersLogo from '@static/ballers-logo.png';
 import facebookIcon from '@icons/facebook-icon.svg';
@@ -9,46 +10,48 @@ import facebookIcon from '@icons/facebook-icon.svg';
  * stored inside for its full operation
  * @returns JSX code to render to the DOM tree
  */
-const Footer = () => (
-  <footer id='footer'>
-    <figure className='footer__logo'>
-      <a href='/'>
-        <img src={ballersLogo} alt="The Ballers' Logo" />
-      </a>
-    </figure>
-    <div className='footer__info'>
-      <div className='footer__links'>
-        <a className='footer__links--item' href=''>
-          Privacy Policy
+const Footer = () => {
+  return (
+    <footer id='footer'>
+      <figure className='footer__logo'>
+        <a href='/'>
+          <img src={ballersLogo} alt="The Ballers' Logo" />
         </a>
-        <a className='footer__links--item' href=''>
-          Terms and Conditions
-        </a>
-        <a className='footer__links--item' href=''>
-          Help
-        </a>
-        <a className='footer__links--item' href=''>
-          Contact Us
-        </a>
-        <a className='footer__links--item' href=''>
-          Accesibility
-        </a>
+      </figure>
+      <div className='footer__info'>
+        <div className='footer__links'>
+          <a className='footer__links--item' href=''>
+            Privacy Policy
+          </a>
+          <a className='footer__links--item' href=''>
+            Terms and Conditions
+          </a>
+          <a className='footer__links--item' href=''>
+            Help
+          </a>
+          <a className='footer__links--item' href=''>
+            Contact Us
+          </a>
+          <a className='footer__links--item' href=''>
+            Accesibility
+          </a>
+        </div>
+        <p className='footer--copyright'>© Copyright {moment().format('YYYY')} The Ballers. All rights reserved.</p>
+        <p className='footer--made-by'>
+          Developed and mantained by{' '}
+          <a href='https://esparev.com' target='_blank' rel='noreferrer noopener'>
+            Esparev
+          </a>
+          .
+        </p>
       </div>
-      <p className='footer--copyright'>© Copyright 2022 The Ballers. All rights reserved.</p>
-      <p className='footer--made-by'>
-        Developed and mantained by{' '}
+      <figure className='footer__social'>
         <a href='https://esparev.com' target='_blank' rel='noreferrer noopener'>
-          Esparev
+          <img src={facebookIcon} alt='Facebook icon' />
         </a>
-        .
-      </p>
-    </div>
-    <figure className='footer__social'>
-      <a href='https://esparev.com' target='_blank' rel='noreferrer noopener'>
-        <img src={facebookIcon} alt='Facebook icon' />
-      </a>
-    </figure>
-  </footer>
-);
+      </figure>
+    </footer>
+  );
+};
 
 export default Footer;
