2e788c88f2419ce0f3ccacdefdb1b3c1b32d2c20
JoseMa <esparev@hotmail.com>
2023-02-22 21:24:16 -0600

    refactor: ♻️ re-design the recovery and email sent pages

--- src/containers/Recovery.jsx
+++ src/containers/Recovery.jsx
@@ -14,7 +14,7 @@ import '@styles/Login.scss';
  */
 const Recovery = () => {
   useEffect(() => {
-    document.title = 'BEISMICH • Recuperar contraseña';
+    document.title = 'Recover Password • The Ballers';
   }, []);
 
   /**
@@ -51,8 +51,8 @@ const Recovery = () => {
       .catch((error) => {
         ReactDOM.render(
           <Message
-            message={`¡Ups!, Hubo un error al enviar el correo. 
-            Verifique que el correo sea el correcto`}
+            message={`Ups!, There was an error sending the email.
+            Verify that the email is correct`}
             messageStatus='error'
           />,
           document.getElementById('message-container')
@@ -62,56 +62,39 @@ const Recovery = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    sendMail(`${envConfig.apiUrl}/auth/recuperar`, form);
+    sendMail(`${envConfig.apiUrl}/auth/recover`, form);
   };
 
   return (
     <>
       <div id='message-container'></div>
 
-      <div className='main-login'>
-        <section className='greeting'>
-          <h1 className='greeting--title'>Recupera tu contraseña</h1>
-        </section>
-        <section className='greeting-desktop'>
-          <h1 className='greeting--title'>Olvidaste tu Contraseña</h1>
-        </section>
-
-        <main className='login-main'>
-          <section className='login'>
-            <div className='login__greet'>
-              <h2 className='login__greet--title'>Olvidaste tu Contraseña</h2>
-              <p className='login__greet--message'>
-                Ingresa el correo electrónico para recuperar tu contraseña
-              </p>
+      <main className='login'>
+        <section className='login'>
+          <div className='login__greet'>
+            <h2 className='login__greet--title'>Forgot Password</h2>
+            <p className='login__greet--message'>Enter your email to recover your password</p>
+          </div>
+          <form className='login__form' onSubmit={handleSubmit}>
+            <div className='login__form--email'>
+              <input
+                className='login__form--input form--input-text'
+                name='email'
+                type='email'
+                placeholder='Email'
+                required
+                onChange={handleInput}
+              />
             </div>
-            <div className='login__greet-desktop'>
-              <h2 className='login__greet--title'>Recupera tu contraseña</h2>
-              <p className='login__greet--message'>
-                Ingresa el correo electrónico para recuperar tu contraseña
-              </p>
-            </div>
-            <form className='login__form' onSubmit={handleSubmit}>
-              <div className='login__form--email'>
-                <input
-                  className='login__form--input form--input-text'
-                  name='email'
-                  type='email'
-                  placeholder='Correo electrónico'
-                  required
-                  onChange={handleInput}
-                />
-              </div>
-              <button type='submit' className='login__form--button'>
-                <Link to='/send-mail'>Enviar</Link>
-              </button>
-            </form>
-            <Link className='login--forgot-password' to='/login'>
-              Regresar a inicio de sesión
-            </Link>
-          </section>
-        </main>
-      </div>
+            <button type='submit' className='login__form--button'>
+              <Link to='/send-mail'>Send mail</Link>
+            </button>
+          </form>
+          <Link className='login--forgot-password' to='/login'>
+            Back to login
+          </Link>
+        </section>
+      </main>
     </>
   );
 };
--- src/containers/SendMail.jsx
+++ src/containers/SendMail.jsx
@@ -4,46 +4,29 @@ import '@styles/Login.scss';
 // ---------------------------------------- END OF IMPORTS
 
 /**
- * Creates the send password recovery mail page with 
+ * Creates the send password recovery mail page with
  * all its functions stored inside for its full operation
  * @returns JSX code to render to the DOM tree
  */
 const SendMail = () => {
   useEffect(() => {
-    document.title = 'BEISMICH • Correo Enviado';
+    document.title = 'Email Sent • The Ballers';
   }, []);
 
   return (
-    <div className='main-login'>
-      <section className='greeting'>
-        <h1 className='greeting--title'>Se ha enviado el correo</h1>
+    <main className='login'>
+      <section className='login'>
+        <div className='login__greet'>
+          <h2 className='login__greet--title'>Email Sent!</h2>
+          <p className='login__greet--message'>Check your inbox to change your password</p>
+        </div>
+        <form className='login__form'>
+          <button className='login__form--button'>
+            <Link to='/login'>Back to login</Link>
+          </button>
+        </form>
       </section>
-      <section className='greeting-desktop'>
-        <h1 className='greeting--title'>¡Correo Enviado!</h1>
-      </section>
-
-      <main className='login-main'>
-        <section className='login'>
-          <div className='login__greet'>
-            <h2 className='login__greet--title'>¡Correo Enviado!</h2>
-            <p className='login__greet--message'>
-              Revisa la bandeja de tu correo para cambiar tu contraseña
-            </p>
-          </div>
-          <div className='login__greet-desktop'>
-            <h2 className='login__greet--title'>Se ha enviado el correo</h2>
-            <p className='login__greet--message'>
-              Revisa la bandeja de tu correo para cambiar tu contraseña
-            </p>
-          </div>
-          <form className='login__form' action=''>
-            <button className='login__form--button'>
-              <Link to='/login'>Iniciar sesión</Link>
-            </button>
-          </form>
-        </section>
-      </main>
-    </div>
+    </main>
   );
 };
 
